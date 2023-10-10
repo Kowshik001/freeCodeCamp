dc50e4ab060dc85f6eda64d01a2b8bb404245cee
BAKI LEELA KOWSHIK REDDY <108167224+Kowshik001@users.noreply.github.com>
2023-10-10 23:10:54 +0530

    Inorder Traversal.py
    This code defines a binary tree and demonstrates the insertion of elements into the tree in a way that maintains the binary search tree property (left child < parent < right child). It also performs an inorder traversal to display the elements in sorted order.
    
    Feel free to modify and extend this code as needed for your specific tree-related program.

---
+++ Inorder Traversal.py
@@ -0,0 +1,29 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+    if root is None:
+        return TreeNode(value)
+    else:
+        if value < root.value:
+            root.left = insert(root.left, value)
+        else:
+            root.right = insert(root.right, value)
+    return root
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.value, end=' ')
+        inorder_traversal(root.right)
+
+root = None
+values = [5, 3, 8, 1, 4, 7, 9]
+for value in values:
+    root = insert(root, value)
+
+print("Inorder Traversal:")
+inorder_traversal(root)
